59ddf1e897f7f1d4427e6ac4365468f8c5c592b9
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-04-21 22:50:37 -0700

    initial commit, creating scaffold for building static portfolio site

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer__container">
+      <div className="footer--right">
+        <h3>Kevin Boyle</h3>
+      </div>
+      <div className="footer--left">
+        <h3 className="footer-left">
+          <span className="display-none-sm-scrn">email me at </span>{" "}
+          boyle.kevin.michael@gmail.com
+        </h3>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import './LandingPage.css'
+
+function LandingPage() {
+  return (
+    <ParallaxProvider>
+      <div>
+        Landing Page
+      </div>
+    </ParallaxProvider>
+  )
+}
+
+export default LandingPage
