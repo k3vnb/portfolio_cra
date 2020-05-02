c6c01af12ca3d85b335877fd9dab11d71ab241a1
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-05-01 21:01:35 -0700

    remove cluttery css properties and styling in about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -40,26 +40,24 @@ const AboutPage = () => {
 
   return (
     <section className="about-container flex-container--centered" ref={ref}>
-      <article style={{ width: "100vw" }}>
-        <div className="about-upper">
-          <div className="about-upper-half about-upper-left flex-container--centered">
-            <span className="hi">Hi</span>
+      <article>
+        <div className="about-container__upper flex-container--centered">
+          <div className="about-container__upper-half about-container__upper--left flex-container--centered">
+            Hi
           </div>
-          <div className="about-upper-half about-upper-right flex-container--centered">
+          <div className="about-container__upper-half about-container__upper--right flex-container--centered">
             <p>
-              Thank you for visiting. I&apos;m Kevin from Portland, OR. I design
-              &amp; develop web apps, websites, and user interfaces.
+              Thank you for visiting. I&apos;m Kevin, I make web apps, websites,
+              &amp; user interfaces.
             </p>
           </div>
         </div>
-        <div className="about-lower">
+        <div className="about-container__lower">
           <p>
-            <span className="leader-text2">
-              Coding is my trade and vehicle for life-long learning.
-            </span>
-            &nbsp;As a programmer, I aim for clean, efficient, maintainable
-            code. As a web developer, I aim to deliver inviting and intuitive
-            virtual spaces with user-friendly applications.
+            Coding is my trade and vehicle for life-long learning. &nbsp;As a
+            programmer, I aim for clean, efficient, maintainable code. As a web
+            developer, I aim to deliver inviting and intuitive virtual spaces
+            with user-friendly applications.
           </p>
           <p>
             My background is in cultural anthropology. I am an artist,
@@ -67,7 +65,7 @@ const AboutPage = () => {
             for programming comes from my interest in the intersection of art
             and technology.
           </p>
-          <p className="leader-text2">My toolkit includes:</p>
+          <p>My toolkit includes:</p>
           <ul className="skills-list ul-reset">
             {skillsList.map((skill) => (
               <li key={skill}>{skill}</li>
